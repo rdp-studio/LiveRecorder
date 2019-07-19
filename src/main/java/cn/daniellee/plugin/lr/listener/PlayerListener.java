d3e09e2dbf17b20dc18d3c9ac7726b4c8fcd1ceb
CNDanielLee <768318841@qq.com>
2019-07-19 16:01:25 +0800

    '1.2.0'

--- src/main/java/cn/daniellee/plugin/lr/listener/PlayerListener.java
+++ src/main/java/cn/daniellee/plugin/lr/listener/PlayerListener.java
@@ -16,37 +16,29 @@ import org.bukkit.event.player.*;
 
 public class PlayerListener implements Listener {
 
-	@EventHandler(priority=EventPriority.LOWEST)
-	public void onPlayerLogin(PlayerLoginEvent e) {
-		String recorderName = LiveRecorder.getInstance().getConfig().getString("setting.recorder-name", "Recorder");
-		if (recorderName.equals(e.getPlayer().getName())) {
-			e.allow();
-		}
-	}
-
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent e) {
         Player player = e.getPlayer();
-        if (player != LiveCore.recorder) {
-	        // 更新活跃玩家列表
-	        ActivePlayer activePlayer = LiveCore.getActivePlayerByName(player.getName());
-	        if (activePlayer != null) {
-		        activePlayer.setLastActive(System.currentTimeMillis());
-	        } else {
-		        activePlayer = new ActivePlayer(player.getName(), System.currentTimeMillis());
-		        LiveCore.activePlayers.add(activePlayer);
-	        }
-        }
-        // 如果正在被录制则移动镜头
-        if (player.getName().equals(LiveCore.recordingPlayer)) {
-	        if (LiveCore.recorder != null && e.getTo() != null) {
-	        	if (LiveCore.recorder.canSee(player)) {
-			        LiveCore.recorder.setVelocity(LiveCore.getVectorByFormTo(e.getFrom(), e.getTo()));
-			        LiveCore.recorder.getWorld().spawnParticle(Particle.VILLAGER_HAPPY, LiveCore.recorder.getLocation(), 0);
-		        } else {
-			        LiveRunnable.resetRecordedSeconds();
-		        }
-	        }
+		// 如果正在被录制则移动镜头
+		if (player.getName().equals(LiveCore.recordingPlayer)) {
+			if (LiveCore.recorder != null && e.getTo() != null) {
+				if (LiveCore.recorder.canSee(player)) {
+					LiveCore.recorder.setVelocity(LiveCore.getVectorByFormTo(e.getFrom(), e.getTo()));
+					LiveCore.recorder.getWorld().spawnParticle(Particle.VILLAGER_HAPPY, LiveCore.recorder.getLocation(), 1);
+				} else {
+					LiveRunnable.resetRecordedSeconds();
+				}
+			}
+		}
+		// 如果是Recorder直接跳过
+        if (LiveCore.recorder != null && player.getName().equals(LiveCore.recorder.getName())) return;
+        // 更新活跃玩家列表
+        ActivePlayer activePlayer = LiveCore.getActivePlayerByName(player.getName());
+        if (activePlayer != null) {
+	        activePlayer.setLastActive(System.currentTimeMillis());
+        } else {
+	        activePlayer = new ActivePlayer(player.getName(), System.currentTimeMillis());
+	        LiveCore.activePlayers.add(activePlayer);
         }
 	}
 
@@ -61,7 +53,7 @@ public class PlayerListener implements Listener {
 	public void onPlayerQuit(PlayerQuitEvent e) {
 		if (e.getPlayer().getName().equals(LiveCore.recordingPlayer)) {
 			LiveRunnable.resetRecordedSeconds();
-		} else if (e.getPlayer() == LiveCore.recorder) {
+		} else if (LiveCore.recorder != null && e.getPlayer().getName().equals(LiveCore.recorder.getName())) {
 			Bukkit.broadcastMessage((LiveRecorder.getInstance().getPrefix() + LiveRecorder.getInstance().getConfig().getString("message.boardcast.offline", "&eThe live recording is over, thanks to the support of the friends~")).replace("&", "§"));
 		}
 	}
